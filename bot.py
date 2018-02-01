06a6fde7a6e1f70a9cc1bfeb4a098c2b6cad3d70
Pedro Henrique <phgp25@gmail.com>
2018-02-01 10:49:58 -0300

    bot finalizado, intervalo de notificacoes configuravel

--- bot.py
+++ bot.py
@@ -9,8 +9,8 @@ API_TELEGRAM_NMPEDIDOBOT_TOKEN = os.environ.get('API_TELEGRAM_NMPEDIDOBOT_TOKEN'
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def detalhe(bot,update,args):
-    order_id = ' '.join(args)
+def pedido_detalhe(num_ped):
+    order_id = ' '.join(num_ped)
     order_detail = order_details(order_id)
     dolar = emojize(":dollar:", use_aliases=True)
     pedido = emojize(":pencil:", use_aliases=True)
@@ -24,6 +24,10 @@ def detalhe(bot,update,args):
         " "+forma_pagamento+" -> "+order_detail[0]['forma_pagamento']+"\n"
         " "+dolar+" -> "+order_detail[0]['valor_total']+"\n"
     )
+    return caption
+
+def detalhe(bot,update,args):
+    caption=pedido_detalhe(args)
     bot.send_message(chat_id=update.message.chat_id,text=caption)
 
 def start(bot, update):
@@ -43,28 +47,68 @@ def help(bot, update):
 
     help_message = (
 
-        "Assim que o bot eh iniciado, ele ja fica fazendo a verificacao se tem pedidos novos.\n\n"
-        "Caso queira saber informacoes de algum pedido especifico, "
-        "voce pode consultar atravez do comando /detelhe <numero_pedido>\n\n"
+        "/set <segundos> para configurar o intervalo de atualizacoes.\n"
+        "/unset para cancelar a atualizacao.\n"
+        "/detalhe <numero_pedido> para saber mais detalhe do pedido.\n"
+
 
     )
     update.message.reply_text(help_message)
 
-def callback_minute(bot,update,job):
-        bot.send_message(chat_id=update.message.chat_id,text='One message every minute')
-# def search_order_loop(bot,job):
-#     orders = list_order_today()
-#     for order in orders:
-#         bot.send_message(chat_id='@NMPedidosBot',text="pedido!")
+def alarm(bot, job):
+    orders_numbers=list_order_today()
+    if(len(orders_numbers)!=0):
+        for order_number in orders_numbers:
+            text = pedido_detalhe(order_number['ID'])
+            bot.send_message(job.context, text=text)
+    else:
+        bot.send_message(job.context, text='Sem novos pedidos')
+
+def set_timer(bot, update, args, job_queue, chat_data):
+    """Add a job to the queue."""
+    chat_id = update.message.chat_id
+    try:
+        # args[0] should contain the time for the timer in seconds
+        due = int(args[0])
+        if due < 0:
+            update.message.reply_text('Sinto muito, mas ainda nao podemos viajar no tempo')
+            return
+
+        # Add job to queue
+        job = job_queue.run_repeating(alarm, due, context=chat_id)
+        chat_data['job'] = job
+
+        update.message.reply_text('Intervalo das notificacoes setado com sucesso')
+
+    except (IndexError, ValueError):
+        update.message.reply_text('Exemplo: /set <seconds>')
+
+
+def unset(bot, update, chat_data):
+    """Remove the job if the user changed their mind."""
+    if 'job' not in chat_data:
+        update.message.reply_text('Sem notificacoes ativadas')
+        return
+
+    job = chat_data['job']
+    job.schedule_removal()
+    del chat_data['job']
+
+    update.message.reply_text('Notificacao cancelada com sucesso')
+
 
 def bot_main():
     updater = Updater(API_TELEGRAM_NMPEDIDOBOT_TOKEN) # Token dado pelo BotFather
     dp = updater.dispatcher
-    j = dp.job_queue
     dp.add_handler(CommandHandler("start", start)) # Manipulador de comandos
     dp.add_handler(CommandHandler("help", help)) # Manipulador de comandos
     dp.add_handler(CommandHandler("detalhe",detalhe,pass_args=True)) # Manipulador de comandos
-    j.run_repeating(callback_minute, interval=10, first=0)
+    dp.add_handler(CommandHandler("set", set_timer,
+                                  pass_args=True,
+                                  pass_job_queue=True,
+                                  pass_chat_data=True))
+    dp.add_handler(CommandHandler("unset", unset, pass_chat_data=True))
+
     updater.start_polling() #Inicia o loop de execução do bot
     updater.idle()
 
